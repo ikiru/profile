06bfa0ec72b25e3c093eccf6cb72b7feb557e9b9
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-06-10 18:36:56 -0500

    jquery added and animation working.

---
+++ js/clouds.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+// animation of clouds off screen left to right
+
+
+  $(function() {
+         $(".cloud1").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '150px',
+            width: '150px'
+        });
+
+  });
+
+});
