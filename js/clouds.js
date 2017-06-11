b1f2a4281142041105134ee29ae843cff4a9ba4d
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-06-11 13:13:20 -0500

    animation not working

--- js/clouds.js
+++ js/clouds.js
@@ -1,15 +1,63 @@
 $(document).ready(function() {
 
-// animation of clouds off screen left to right
+  // animation of clouds off screen left to right
 
 
   $(function() {
-         $(".cloud1").animate({
-            left: '250px',
-            opacity: '0.5',
-            height: '150px',
-            width: '150px'
-        });
+
+// ----------------------- space -----------------------------------------------
+
+    animateDiv();
+
+  });
+
+  function makeNewPosition() {
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+  }
+
+  function animateDiv() {
+    var newq = makeNewPosition();
+    $('.ufo').animate({
+      top: newq[0],
+      left: newq[1]
+    }, function() {
+      animateDiv();
+    });
+
+
+
+
+// ----------------------- sky -------------------------------------------------
+    $(".cloud1").animate({
+      left: '2000px'
+    }, 100000, 'linear');
+    $(".cloud2").animate({
+      left: '2000px'
+    }, 115000, 'swing');
+    $(".balloon").animate({
+      left: '2000px'
+    }, 70000, 'swing');
+
+
+// ----------------------- Land ------------------------------------------------
+
+
+
+// ----------------------- Sea -------------------------------------------------
+
+
+
+// ----------------------- Deep Sea --------------------------------------------
+
 
   });
 
